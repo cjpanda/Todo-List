c693c6af4ad41e907229b62ad52474a383189fe9
cjpanda <verifiedceejay@gmail.com>
2023-08-09 14:39:48 +0100

    Task list

--- src/App.js
+++ src/App.js
@@ -3,46 +3,55 @@ import SearchTask from './SearchTask';
 import AddTask from './AddTask';
 import Tasks from './Tasks';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasklist')));
+  //Database URL
+  const API_URL = "http://localhost:3500/tasks";
 
-const [newTask, setNewTask] = useState ('')
-const [search, setSearch] = useState ('')
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState ('')
+  const [search, setSearch] = useState ('')
 
-const  setAndSaveTasks = (newTasks) => {
-  setTasks(newTasks);
-  localStorage.setItem('tasklist', JSON.stringify(newTasks));
-}
+// USe Effect Syncs the Database at load time to The Currently State  using async/await
+  useEffect(() => {
+    const fetchTasks = async ()=> {
+      try {
+        const response = await fetch(API_URL);
+        const listTasks = await response.json();
+        console.log(listTasks)
+        setTasks(listTasks)
+      } catch (error) {
+        console.log(error.stack)
+        
+      }
+    }
+    (async () => await fetchTasks())();
+  }, [])
 
-const addTask = (task) => {
-  const id = tasks.length ? tasks[tasks.length - 1].id + 1 : 1;
-  const myNewTask = {id, checked: false, task};
-  const listTasks = [...tasks, myNewTask];
-  setAndSaveTasks(listTasks);
-}
+  const addTask = (task) => {
+    const id = tasks.length ? tasks[tasks.length - 1].id + 1 : 1;
+    const myNewTask = {id, checked: false, task};
+    const listTasks = [...tasks, myNewTask];
+    setTasks(listTasks);
+  }
+// HandleCheck Function checks 
+  const handleCheck = (id) => {
+      const listTasks = tasks.map((task) => task.id === id ? {...task, checked: !task.checked } : task );
+      setTasks(listTasks);    
+  }
 
-const handleCheck = (id) => {
-    const listTasks = tasks.map((task) => task.id === id ? {...task, checked: !task.checked } : task );
-    setAndSaveTasks(listTasks);
-    
-}
+  const handleDelete = (id) => {
+      const listTasks = tasks.filter((task) => task.id !== id);
+      setTasks(listTasks);
+  }
 
-const handleDelete = (id) => {
-    const listTasks = tasks.filter((task) => task.id !== id);
-    setAndSaveTasks(listTasks);
-
-    
-
-}
-
-const handleSubmit =(e) => {
-  e.preventDefault(); 
-  if (!newTask) return;
-  addTask(newTask);
-  setNewTask("");
-}
+  const handleSubmit =(e) => {
+    e.preventDefault(); 
+    if (!newTask) return;
+    addTask(newTask);
+    setNewTask("");
+  }
   
   return (
     <div className="App">
